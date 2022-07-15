f284ccce2c49b098493d8e2764c8781bfb765f0c
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-07-16 05:26:18 +0600

    Recent Section Title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero';
 import Featured from './../Featured/Featured';
+import Recent from '../Recent/Recent';
 
 const Home = () => {
   return (
     <>
        <Hero />
        <Featured />
+       <Recent />
     </>
   )
 }
---
+++ src/components/Recent/Recent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+const Recent = () => {
+  return (
+    <>
+        <section className="recent">
+            <div className="container">
+                <SectionTitle 
+                    title="Recent Property Listed" 
+                    subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+                />
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Recent
