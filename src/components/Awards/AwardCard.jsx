62c83d985776f785f23a30d7d6518230ddff9b6b
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-07-16 09:10:30 +0600

    Awards

---
+++ src/components/Awards/AwardCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import AwardInfo from '../../data/AwardInfo.json';
+
+const AwardCard = () => {
+    return (
+        <div className="awards__content grid-4">
+            {
+                AwardInfo.map((AwItem, index) => {
+                    return (
+                        <div className="awards__box " key={index}>
+                            <div className="awards__icon">
+                                <span>
+                                    <i className={AwItem.icon}></i>
+                                </span>
+                                <h2>{AwItem.num}</h2>
+                                <p>{AwItem.name}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default AwardCard
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero/Hero';
 import Featured from './../Featured/Featured';
 import Recent from '../Recent/Recent';
+import Awards from '../Awards/Awards';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
        <Hero />
        <Featured />
        <Recent />
+       <Awards />
     </>
   )
 }
